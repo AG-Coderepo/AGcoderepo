6cab0873f61d5702a1437dffb82e1df5628a9ccc
NR, Athmeeya <Athmeeya.NR@emc.com>
2025-04-11 20:04:27 +0530

    Create building_better_calc.py
    Commit

---
+++ Practice/building_better_calc.py
@@ -0,0 +1,9 @@
+num1 = float(input("Please enter num1 : "))
+num2 = float(input("Please enter num2 : "))
+op = input("Please enter the operator : ")
+if op == "+" :
+    print(num1+num2)
+elif op == "*" :
+    print(num1*num2)
+else:
+    print("Invalid operator")
